d9c9f351797c0def832625a8769ff099d024f644
jadie <jadeczjade008@gmail.com>
2026-01-19 19:07:17 +0100

    chore: deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,13 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven(url = "https://jitpack.io")
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("com.github.Jadiefication:Void:v2.1.0")
+    implementation("com.github.Jadiefication:Void-HTML:v1.1.0")
 }
 
 kotlin {
